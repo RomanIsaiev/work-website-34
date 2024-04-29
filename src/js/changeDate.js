05fccabb38fb445ab4f26baf715a61621b4fdadb
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-04-30 00:44:09 +0300

    change date

---
+++ src/js/changeDate.js
@@ -0,0 +1,46 @@
+// change day
+
+var currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 1);
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var year = currentDate.getFullYear();
+var formattedDate = day.toString().padStart(2, '0');
+
+document.getElementById('dateElement').innerHTML = formattedDate;
+
+// **************************************************************************
+
+var current_date = new Date();
+current_date.setDate(current_date.getDate() + 1);
+var month = new Array(
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12'
+);
+
+// var month = new Array(
+//   'СІЧНЯ',
+//   'ЛЮТОГО',
+//   'БЕРЕЗНЯ',
+//   'КВІТНЯ',
+//   'ТРАВНЯ',
+//   'ЧЕРВНЯ',
+//   'ЛИПНЯ',
+//   'СЕРПНЯ',
+//   'ВЕРЕСНЯ',
+//   'ЖОВТНЯ',
+//   'ЛИСТОПАДА',
+//   'ГРУДНЯ'
+// );
+document.getElementById('monthElement').innerHTML =
+  month[current_date.getMonth()];
